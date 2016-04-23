b7cef7e33be6569a26cef88275d5473881b170b9
Herman Banken <hermanbanken@gmail.com>
2016-04-24 01:58:39 +0200

    Use Meteor's users as devices

--- server/client/main.js
+++ server/client/main.js
@@ -5,23 +5,7 @@ import './main.html';
 Devices = new Mongo.Collection('devices');
 
 Template.main.helpers({
-	devices: () => Devices.find()
+	devices: () => Meteor.users.find({ 
+		"profile.owner": { "$exists": 1} 
+	})
 })
-
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
--- server/server/main.js
+++ server/server/main.js
@@ -1,22 +1,25 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base'
 
 Devices = new Mongo.Collection('devices');
 
 Meteor.startup(() => {
   // code to run on server at startup
-  if(Devices.find().count() == 0) {
-  	Devices.insert({
-  		name: "Lisa",
-  		owner: "Martijn",
-  		online: true,
-  		lastPing: new Date() 
-  	})
-  	Devices.insert({
-  		name: "Thomas",
-  		owner: "Herman",
-  		online: false,
-  		lastPing: new Date()
-  	})
-  }
+  ["thomas@edison", "lisa@edison", "gui@local"]
+    .filter(name => !Meteor.users.findOne({ username: name.split("@")[0] }))
+    .forEach(name => {
+      Accounts.createUser({
+        username: name.split("@")[0],
+        email: name,
+        password: name.split("@")[0],
+        profile: { name: name.split("@")[0] }
+      })
+    });
 
+  [['thomas', 'Herman'], ['lisa', 'Martijn']]
+    .forEach(function(u){
+      Meteor.users.update({ username: u[0] }, { 
+        $set: { "profile.owner": u[1] }
+      });
+    });
 });
