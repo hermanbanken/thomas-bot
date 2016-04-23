b6b2de3795be0e5bdc44e21c14e4d946c254f074
Herman Banken <hermanbanken@gmail.com>
2016-04-23 23:25:31 +0200

    Add basic device list

--- server/server/main.js
+++ server/server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 
+Devices = new Mongo.Collection('devices');
+
 Meteor.startup(() => {
   // code to run on server at startup
+  if(Devices.find().count() == 0) {
+  	Devices.insert({
+  		name: "Lisa",
+  		owner: "Martijn",
+  		online: true,
+  		lastPing: new Date() 
+  	})
+  	Devices.insert({
+  		name: "Thomas",
+  		owner: "Herman",
+  		online: false,
+  		lastPing: new Date()
+  	})
+  }
+
 });
