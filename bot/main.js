859c661a2f05495630cafc8dd336503926dac0b3
Herman Banken <hermanbanken@gmail.com>
2016-04-24 03:07:02 +0200

    Speak at start of Edison

--- bot/main.js
+++ bot/main.js
@@ -8,4 +8,16 @@ auth({ host : "10.10.107.39" }).then(function(ddp){
   flex(3, function(){
     console.log("Flex motion!");
   })
+
+  ddp.call(
+      'speak',
+      ["Ik ben opgestart! Getekend, Thomas.", "Xander"],
+      function (err, result) {   // callback which returns the method call results 
+        console.log('called function, result: ' + result);
+      },
+      function () {              // callback which fires when server has finished 
+        console.log('updated');  // sending any updated documents as a result of 
+        console.log(ddp.collections.users);  // calling this method 
+      }
+    );
 }).done();
